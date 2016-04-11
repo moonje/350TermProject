55dfb6382d2aa626064d25a5d85471cd6c6f0d43
zelaskoj <zelaskoj@mail.gvsu.edu>
2016-04-11 00:21:35 -0400

    Fixed all panel add issues with tabs

--- src/ModelBoard2.java
+++ src/ModelBoard2.java
@@ -11,26 +11,16 @@ import javax.swing.*;
  **********************************************************************/
 public class ModelBoard2 extends JFrame implements ActionListener{
 	
-	private JMenuBar menus; 
-<<<<<<< HEAD
+	private JMenuBar menus;
 	private JMenu optionMenu;
-	private JMenu chessMenu;
-	private JMenu checkersMenu;
-	private JMenuItem resignChess;
-	private JMenuItem newChessGame;
-	private JMenuItem resignCheckers;
-	private JMenuItem newCheckersGame;
-=======
 	private JMenu chessoptionMenu;
-	private JMenu checkoptionMenu; 
+	private JMenu checkoptionMenu;
 	private JMenuItem resignChess; 
 	private JMenuItem newChessGame;
-	private JMenuItem newCheckGame; 
-	private JMenuItem resignCheck; 
->>>>>>> 364e967dd7d787df70d1b818d109851fa0830a03
+	private JMenuItem resignCheck;
+	private JMenuItem newCheckGame;
 	private ModelBoard model; 
-	private CheckersPanel check; 
-	private JPanel panel;
+	private CheckersPanel check;
 	private JTabbedPane tabbedPane;
 
 	/*******************************************************************
@@ -49,11 +39,10 @@ public class ModelBoard2 extends JFrame implements ActionListener{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		model = new ModelBoard();
 		check = new CheckersPanel();
-		//panel = new JPanel();
         tabbedPane = new JTabbedPane();
 
-		tabbedPane.addTab("Chess", null, model, "Current Chess game");
         tabbedPane.addTab("Checkers", null, check, "Current Checkers game");
+		tabbedPane.addTab("Chess", null, model, "Current Chess game");
 
 		this.getContentPane().add(tabbedPane);
 		
@@ -66,29 +55,17 @@ public class ModelBoard2 extends JFrame implements ActionListener{
 	 * Creates the Menus
 	 ******************************************************************/
 	private void setupMenus(){
-<<<<<<< HEAD
 		optionMenu = new JMenu("Options");
 
-		chessMenu = new JMenu("Chess");
-		checkersMenu = new JMenu("Checkers");
+		chessoptionMenu = new JMenu("Chess");
+		checkoptionMenu = new JMenu("Checkers");
 
 		resignChess = new JMenuItem("Resign Chess");
 		newChessGame = new JMenuItem("New Chess Game");
-		resignCheckers = new JMenuItem("Resign Checkers");
-		newCheckersGame = new JMenuItem("New Checkers Game");
 		
 		resignChess.addActionListener(this);
 		newChessGame.addActionListener(this);
-		resignCheckers.addActionListener(this);
-		newCheckersGame.addActionListener(this);
 
-		optionMenu.add(chessMenu);
-		chessMenu.add(resignChess);
-		chessMenu.add(newChessGame);
-		optionMenu.add(checkersMenu);
-		checkersMenu.add(resignCheckers);
-		checkersMenu.add(newCheckersGame);
-=======
 		chessoptionMenu = new JMenu("Chess");
 		checkoptionMenu = new JMenu("Checkers");
 		resignChess = new JMenuItem("Resign Chess");
@@ -106,11 +83,10 @@ public class ModelBoard2 extends JFrame implements ActionListener{
 		
 		checkoptionMenu.add(resignCheck);
 		checkoptionMenu.add(newCheckGame);
->>>>>>> 364e967dd7d787df70d1b818d109851fa0830a03
 		
 		menus = new JMenuBar();
-		menus.add(chessoptionMenu);
 		menus.add(checkoptionMenu);
+		menus.add(chessoptionMenu);
 		setJMenuBar(menus);
 	}
 
@@ -146,31 +122,7 @@ public class ModelBoard2 extends JFrame implements ActionListener{
 				this.tabbedPane.remove(model);
 				this.remove(model);
 				model = new ModelBoard();
-				this.tabbedPane.insertTab("Chess", null, model, "Current Chess game", 0);
-				this.tabbedPane.setSelectedIndex(0);
-				this.setVisible(true);
-			}
-		}
-		if (e.getSource() == resignCheckers){
-			int response = JOptionPane.showConfirmDialog(null,
-					"Are you sure you would like to resign?",
-					null,
-					JOptionPane.YES_NO_OPTION);
-
-			if (response == JOptionPane.YES_OPTION)
-				check.disableBoard(false);
-		}
-		if (e.getSource() == newCheckersGame){
-			int response = JOptionPane.showConfirmDialog(null,
-					"Are you sure you would like to start a new game?",
-					null,
-					JOptionPane.YES_NO_OPTION);
-
-			if (response == JOptionPane.YES_OPTION){
-				this.tabbedPane.remove(check);
-				this.remove(check);
-				check = new CheckersPanel();
-				this.tabbedPane.insertTab("Checkers", null, check, "Current Checkers game", 1);
+				this.tabbedPane.insertTab("Chess", null, model, "Current Chess game", 1);
 				this.tabbedPane.setSelectedIndex(1);
 				this.setVisible(true);
 			}
@@ -194,11 +146,13 @@ public class ModelBoard2 extends JFrame implements ActionListener{
 				    + " of Checkers?",
 				    null,
 				    JOptionPane.YES_NO_OPTION);
-			
+
 			if (response == JOptionPane.YES_OPTION){
+				this.tabbedPane.remove(check);
 				this.remove(check);
 				check = new CheckersPanel();
-				this.getContentPane().add(check);
+				this.tabbedPane.insertTab("Checkers", null, check, "Current Checkers game", 0);
+				this.tabbedPane.setSelectedIndex(0);
 				this.setVisible(true);
 			}
 		}
